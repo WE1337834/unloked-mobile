e1d135d2499905a2d4c5965f64e448c97193a295
WE1337934 <kuzya.mironov.04@gmail.com>
2024-09-12 16:07:54 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import uiautomator2 as u2
+
+device = "adb-af38ae38-H0UxOX._adb-tls-connect._tcp"
+
+def Password():
+    global symbol_pass
+    symbol_pass = []
+    password = input("Введите пароль: ")
+    symbol_pass += list(map(str, *f"{password}".split(" ")))
+
+def unlock_mobile(symbol_pass):
+    d = u2.connect(device)
+    d.app_current()     
+    d.swipe_ext("up")
+    d.sleep(1)
+    for i in range(0,len(symbol_pass)):
+        d(text=symbol_pass[i]).click()
+Password()
+unlock_mobile(symbol_pass)
